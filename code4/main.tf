2370d474d9e988fe80384dd5197903a85473d396
dombledort <maurelkouam2@gmail.com>
2025-02-02 06:40:02 +0100

    class5 code

---
+++ code4/main.tf
@@ -0,0 +1,27 @@
+resource "aws_lightsail_instance" "custom" {
+  name              = "myserver"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  user_data         = file("install.sh")
+  key_pair_name     = "milo2"
+  tags = {
+    Team      = "Devops"
+    env       = "dev"
+    create_by = "terraform"
+  }
+}
+
+resource "aws_lightsail_instance" "custom1" {
+  name              = "myserver2"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  user_data         = file("install.sh")
+  key_pair_name     = "milo2"
+  tags = {
+    Team      = "Devops"
+    env       = "dev"
+    create_by = "terraform"
+  }
+}
